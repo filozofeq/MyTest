74b592656fd27606c747b8d47564670951c4a2e7
Adam Krawczyk <filozofeq@gmail.com>
2015-10-26 14:02:52 +0100

    Add new file

---
+++ server.js
@@ -0,0 +1,6 @@
+var http = require('http')
+var port = process.env.PORT || 3500;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello on the training presentation! :)\n');
+}).listen(port);
