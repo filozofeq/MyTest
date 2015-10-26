84e0ee57bfb6930c706787f250046727ca59aea1
Adam Krawczyk <filozofeq@gmail.com>
2015-10-26 14:04:10 +0100

    Update short description into app

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ var http = require('http')
 var port = process.env.PORT || 3500;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello on the training presentation! :)\n');
+  res.end('Hello world on the training presentation! :)\n');
 }).listen(port);
