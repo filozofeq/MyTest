c63047ec54edd3afff9ae972147713fbcc52b373
Adam Krawczyk <filozofeq@gmail.com>
2015-10-26 14:46:15 +0100

    Upload text to show current time

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 var http = require('http')
-var port = process.env.PORT || 3500;
+var port = process.env.PORT || '3500';
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Hello world on the training presentation! :)\n');
+  var todayTime = new Date().toLocaleTimeString( );
+  res.end('Time on the server is: ' + todayTime);
 }).listen(port);
+console.log('Server is now running on port ' + port);
